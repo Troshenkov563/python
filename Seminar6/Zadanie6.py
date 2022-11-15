d1f8177279a396145b4f8f4dd6fc588e02f787be
Troshenkov_Ivan <troshenkov.ii@gmail.com>
2022-11-15 18:29:28 +0300

    dz

--- Seminar6/Zadanie6.py
+++ Seminar6/Zadanie6.py
@@ -32,3 +32,4 @@ print(fib(int(input())))
 numbers = ((10, 10, 10, 12), (30, 45, 56, 45), (81, 80, 39, 32), (1, 2, 3, 4), (90, 10))
 my_list = [sum(i)/len(i) for i in numbers] #list comprehension с изменением
 print(my_list)
+
