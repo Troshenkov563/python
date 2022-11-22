99dc804a6f5524c6ac69897565b2faa7c854f5ba
Troshenkov_Ivan <troshenkov.ii@gmail.com>
2022-11-22 16:04:57 +0300

    calc

--- Seminar7/logger.py
+++ Seminar7/logger.py
@@ -4,6 +4,6 @@ from view import Get_mode
 
 def calc_logger(num1, num2, oper, res):
     time = dt.now().strftime('%H:%M')
-    with open('Seminar71\log.txt', 'a', encoding='utf-8') as file:
+    with open('Seminar7\log.txt', 'a', encoding='utf-8') as file:
         file.write('{}; число 1: {};число 2: {}; операция: {}; результат: {};\n'
                     .format(time, num1, num2, oper, res))
